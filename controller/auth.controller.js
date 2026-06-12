8ceec1982f66850620432e7389266170629df999
Nicholas <danielnick210@gmail.com>
2026-06-12 11:07:07 +0100

    fixed the controller

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,1 +1,14 @@
-import { createTable } from "../models/auth.model.js";
+import bcrypt from "bcrypt";
+import { createUser, findUserByEmail } from "../models/auth.model.js";
+
+export const authentification = async (req, res) => {
+	const { username, email, password } = req.body;
+
+	try {
+		const hashesPassword = await bcrypt.hash(password, 10);
+		const result = await createUser(username, email, hashesPassword);
+		res.status(201).json({ message: "created successfully", result: result });
+	} catch (err) {
+		res.json(err);
+	}
+};
--- models/auth.model.js
+++ models/auth.model.js
@@ -20,16 +20,19 @@ export const createTable = async () => {
 		// 	await pool.end()
 	}
 };
+createTable();
 
 export const createUser = async (username, email, password) => {
 	const result = await pool.query(
-			`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`,
-			[username, email, password]
-	),
-	return result.rows[0]
+		`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`,
+		[username, email, password],
+	);
+	return result.rows[0];
 };
 
 export const findUserByEmail = async (email) => {
-	const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [email])
-	return result.rows[0]
-}
+	const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+		email,
+	]);
+	return result.rows[0];
+};
