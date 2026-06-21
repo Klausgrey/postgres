3b03c74cc8ab4e0e130bd57e445e31cb21b16628
Nicholas <danielnick210@gmail.com>
2026-06-21 08:02:03 +0100

    learning websockets

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import "./config/redis.js";
+import { fileURLToPath } from "url";
+import path from "path";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/post", postRouter);
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
 
 export default app;
--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
 import app from "./app.js";
 
-app.listen(3000, () => {
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+	console.log("a user connected:", socket.id);
+
+	socket.on("message", (msg) => {
+		io.emit("message", msg); // send to ALL connected users
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected:", socket.id);
+	});
+});
+
+httpServer.listen(3000, () => {
 	console.log("running");
 });
